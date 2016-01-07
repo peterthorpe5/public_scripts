e32926317f497257986bee0797ce383a1192e49e
Peter Thorpe <peter.thorpe@hutton.ac.uk>
2016-01-07 14:31:32 +0000

    primer design program with args

--- primer_designer/Primer_program_using_classes_work_in_progressm.py
+++ primer_designer/Primer_program_using_classes_work_in_progressm.py
@@ -1,13 +1,11 @@
 from __future__ import division
 from string import maketrans
-import os
-import sys
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
-from Bio import SeqIO
-from optparse import OptionParser
+#needed for at calc
+#from calculate_at import calculate_at #not used anymore, this was added to the class
 
+#from primer_excersive import * #not used anymore, this was added to the class
 ###################################################################################################
+
 # make a class to add the primer info
 
 class Primerrecord(object):
@@ -140,113 +138,58 @@ To call the class get the AT content of the sequence -
 """
 
 ###################################################################################################
-#testing the code
+#run the code
 
-##DNA ="ACGTGCATGCATGCATGCATTTGATAGATGTAGACGATGCATGCATGCATGCATGC"
-##DNA = DNA.upper()
-##list_of_all_possible_bases = ["A", "T", "C", "G", "N"]
-##
-##count = 0
-##for i in DNA:
-##    count = count+1
-##    assert i in list_of_all_possible_bases, "found a base that is not a nucleotide base, %s at position %d" %(i, count)
-##
-##
-### add list infront of the generator expression to convert to a list
-##primer_list= list(all_possible_primers(DNA, k=15))
-##
-###make list of left and right primers
-##Forward_primer_list = []
-##Rev_primer_list = []
-##
-### way to iterate through the class object
-##print "primer_info\tcoordinate_in_seq\tprimer_sequence\tAT_content\tMelting_temp\tPalindromic_mismatches\tpercentage_non_palidromic"
-##for i in primer_list:
-##    if i.is_palindromic():
-##        print "is_palindromic"
-##    #print "mismatches = ", i.mismatches()
-##    percentage_non_palidromic = 100*(i.mismatches()/float(len(i.sequence)))
-##    at = 100*(i.get_AT())
-##    print "%s\t%d\t%s\t%.1f\t%d\t%d\t%.1f" %(i.primer_info, i.coordinate, i.sequence, at,i.melting_temp(),\
-##                                              i.mismatches(), percentage_non_palidromic)
-##    # get specific bits of info from the class by variable.class_field
-##    #print i.primer_info
-##    if i.primer_info.startswith("F"):
-##        Forward_primer_list.append(i)
-##    else:
-##        Rev_primer_list.append(i)
-##    #print('at content is : ' + str(i.get_AT()))
-##    #print primer_data.primer_info
-##
-###print primer_data.primer_info
-###print Forward_primer_list
-
-####################################################################################################################################################
-
-if "-v" in sys.argv or "--version" in sys.argv:
-    print "v0.0.2"
-    sys.exit(0)
+DNA ="ACGTGCATGCATGCATGCATTTGATAGATGTAGACGATGCATGCATGCATGCATGC"
+DNA = DNA.upper()
 
 
-usage = """Use as follows:
+#this one breaks the code due to non nucleotide base. 
+#DNA ="ArCGTGCATGCATGCATGCATTTGATAGATGTAGACGATGCATGCATGCATGCATGC"
 
-converts
+list_of_all_possible_bases = ["A", "T", "C", "G", "N"]
 
-$ python Primer_program_using_classes.py -i fasta_file_in -k kmer_length_of_primer -o out.results
+count = 0
+for i in DNA:
+    count = count+1
+    assert i in list_of_all_possible_bases, "found a base that is not a nucleotide base, %s at position %d" %(i, count)
 
 
-Program will make all the possible primer combinations for a given sequence, or file of sequences. 
-loop over all record in fasta and creates an individual file of primers for each seq ...
+# add list infront of the generator expression to convert to a list
+primer_list= list(all_possible_primers(DNA, k=15))
 
 
-The info provided is:
-primer_orientation, coordinate in seq, AT content, melting_temp, palindromic mismatches (greater the better!!!
-"""
+#make list of left and right primers
+Forward_primer_list = []
+Rev_primer_list = []
 
-parser = OptionParser(usage=usage)
+# way to iterate through the class object
+print "primer_info\tcoordinate_in_seq\tprimer_sequence\tAT_content\tMelting_temp\tPalindromic_mismatches\tpercentage_non_palidromic"
 
-parser.add_option("-i", "--fasta", dest="in_file", default=None,
-                  help="fasta file of nucleotide seq you want to desin primers for")
-parser.add_option("-k", "--kmer", dest="kmer", default="23",
-                  help="length of primers required")
-parser.add_option("-o", "--out", dest="out_file", default="primer_results.out",
-                  help="prefix for outfile",
-                  metavar="FILE")
+for i in primer_list:
+    NOTES = ""
+    if i.is_palindromic():
+        NOTES = "IS_PALINDROMIC"
+
+    #print "mismatches = ", i.mismatches()
+    percentage_non_palidromic = 100*(i.mismatches()/float(len(i.sequence)))
+    at = 100*(i.get_AT())
+    print "%s\t%d\t%s\t%.1f\t%d\t%d\t%.1f\t%s" %(i.primer_info, i.coordinate, i.sequence, at,i.melting_temp(),\
+                                              i.mismatches(), percentage_non_palidromic, NOTES)
+    # get specific bits of info from the class by variable.class_field
+    #print i.primer_info
+    if i.primer_info.startswith("F"):
+        Forward_primer_list.append(i)
+    else:
+        Rev_primer_list.append(i)
+    #print('at content is : ' + str(i.get_AT()))
+    #print primer_data.primer_info
+
+#print primer_data.primer_info
+#print Forward_primer_list
 
 
 
 
-(options, args) = parser.parse_args()
-
-in_file = options.in_file
-kmer = options.kmer
-out_file = options.out_file
-
-###################################################################################################################################################
-# loop over all record in fasta and creates an individual file of primers for each seq. 
-for seq_record in SeqIO.parse(in_file, "fasta"):
-    DNA = str(seq_record.seq.upper())
-    k = int(kmer)
-    out_file_name = seq_record.id.replace(" ", "_")+"_"+out_file
-    primer_list= list(all_possible_primers(DNA, k))
-    f_out = open(out_file_name, "w")
-    title = "primer_info\tstart_coordinate\tend_coordinate_in_seq\tprimer_sequence\tAT_content_%\tMelting_temp\tPalindromic_mismatches\tpercentage_non_palidromic_%\tNOTES\n"
-    f_out.write(title)
-    for i in primer_list:
-        NOTES = ""
-        if i.is_palindromic():
-            NOTES = "IS_PALINDROMIC"
-        #print "mismatches = ", i.mismatches()
-        percentage_non_palidromic = 100*(i.mismatches()/float(len(i.sequence)))
-        if percentage_non_palidromic == 0.0:
-            NOTES = "IS_PALINDROMIC"
-        at = 100*(i.get_AT())
-        start = 1+i.coordinate -k
-        data_formatted = "%s\t%d\t%d\t%s\t%.1f\t%d\t%d\t%.1f\t%s\n" %(i.primer_info, start,i.coordinate, \
-                                                                      i.sequence, at,i.melting_temp(),\
-                                                                      i.mismatches(), percentage_non_palidromic, NOTES)
-        f_out.write(data_formatted)
-
-    f_out.close()
 
     
