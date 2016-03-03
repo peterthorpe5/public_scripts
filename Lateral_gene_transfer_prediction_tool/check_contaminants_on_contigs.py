33ac1395242f1244634accb417a3150191e77466
Peter Thorpe <peter.thorpe@hutton.ac.uk>
2016-03-03 17:55:41 +0000

    expression exons added

--- Lateral_gene_transfer_prediction_tool/check_contaminants_on_contigs.py
+++ Lateral_gene_transfer_prediction_tool/check_contaminants_on_contigs.py
@@ -37,6 +37,7 @@ def parse_gff(gff):
         scaffold,a,b,c,d,e,f,g,gene_info = line.split("\t")
         gene = gene_info.replace(";", "").split("ID=")[1]
         gene = gene.split(".gene")[0]
+        gene = gene.split(".exon")[0]
         gene = gene.rstrip("\n")
         #scaffold_to_gene_dict[scaffold]=gene.rstrip("\n")
         if not scaffold in scaffold_to_gene_dict:
@@ -46,6 +47,32 @@ def parse_gff(gff):
     #print scaffold_to_gene_dict
     f_in.close()
     return scaffold_to_gene_dict
+
+def gene_to_exon(gff):
+    "function to parse GFF and produce a scaffold_to_gene_dict"
+    f_in = open(gff, "r")
+    # data to output of function
+    gene_to_exon_count = dict()
+    count = 1 
+    #iterate through gff
+    for line in f_in:
+        if line.startswith("#"):
+            continue
+        if not line.split("\t")[2] == "exon":
+            continue
+        scaffold,a,b,c,d,e,f,g,gene_info = line.split("\t")
+        gene = gene_info.replace(";", "").split("ID=")[1]
+        gene = gene.split(".gene")[0]
+        gene = gene.split(".exon")[0]
+        gene = gene.rstrip("\n")
+        #scaffold_to_gene_dict[scaffold]=gene.rstrip("\n")
+        if not gene in gene_to_exon_count:
+            gene_to_exon_count[gene] = count
+        else:
+            gene_to_exon_count[gene]= count +1
+    #print scaffold_to_gene_dict
+    f_in.close()
+    return gene_to_exon_count
 
 def LTG_file(LTG):
     """function to parse LTG prediction
@@ -61,19 +88,83 @@ def LTG_file(LTG):
             continue
         gene = line.split("\t")[0]
         comment = line.split("\t")[-1]
+        kingdom = line.split("\t")[5]
         gene = gene.replace("ID=", "").split("gene=g")[0]
         gene = gene.rstrip()
         if not ".t" in gene:
             gene = gene.replace("t", ".t")
         HGT_predicted_gene_set.add(gene)
-        gene_to_comment_dict[gene] = comment.rstrip("\n")
+        data_out_formatted = "%s\t%s" %(comment.rstrip("\n"), kingdom)
+        gene_to_comment_dict[gene] = data_out_formatted
     #print HGT_predicted_gene_set
     f_in.close()
     return HGT_predicted_gene_set, gene_to_comment_dict
 
+def get_stats_on_AT_content(dna_file):
+    """function to get the mean at standard dev for AT content across
+    all genes"""
+    from Bio.Seq import Seq
+    from Bio.SeqRecord import SeqRecord
+    from Bio import SeqIO
+    import numpy
+    gene_AT_cont_dic = dict()
+
+    AT_content_list = []
+
+    for seq_record in SeqIO.parse(dna_file, "fasta"):
+        seq_record.seq = seq_record.upper()
+        sequence = str(seq_record.seq)
+        a_count = sequence.count('A')
+        t_count = sequence.count('T')
+        #count AT content
+        AT = a_count+t_count/len(seq_record.seq)
+        #put that in a list 
+        AT_content_list.append(AT)
+        # assign AT to the gene name for testing later
+        gene_AT_cont_dic[seq_record.id] = AT
+    #calc average AT for all gene
+    the_mean = sum(AT_content_list) / float(len(AT_content_list))
+    #calc SD for AT for all genes
+    standard_dev = numpy.std(AT_content_list)
+    return gene_AT_cont_dic, the_mean, standard_dev
+
+def parse_rnaseq(rnaseq):
+    "parse the rnaseq-file"
+
+    gene_to_expression = dict()
+    with open(rnaseq, "r") as handle:
+        for line in handle:
+            if line.startswith("#"):
+                continue
+            Name, Length, TPM, NumReads = line.rstrip("\n").split()
+            gene_to_expression[Name]= TPM
+    return gene_to_expression
+
+
+def check_HGT_AT_vs_global_AT(gene_AT_cont_dic, the_mean, standard_dev,
+                              gene_of_interest, comment, sd_numbers, gene_to_expression,
+                              gene_to_exon_count):
+    """function to check the AT content of a gene of interest vs the global
+    AT using the mean and SD already generated"""
+    # user defined number of standard deviations away from the mean for the stats
+    sd_numbers = float(sd_numbers)
+    current_gene_AT = gene_AT_cont_dic[gene_of_interest]
+    #print "gene_of_interest %s has AT cont of %d" %(gene_of_interest, current_gene_AT)
+    lower_threshold = float(the_mean) - (sd_numbers*float(standard_dev))
+    upper_threshold = float(the_mean) + (sd_numbers*float(standard_dev))
+    #print lower_threshold, upper_threshold
+    
+    if current_gene_AT < lower_threshold or current_gene_AT > upper_threshold:
+        #call dict to get expression
+        TPM = gene_to_expression[gene_of_interest]
+        exons = gene_to_exon_count[gene_of_interest]
+        print "gene: %s\tAT_cont: %d\tcomment: ...%s... \texpression: %s\texons: %d"\
+                %(gene_of_interest, current_gene_AT, comment, TPM, exons)
+        
+
 # main function
 
-def check_scaffolds_for_only_HGT_genes(gff, LTG, out_file):
+def check_scaffolds_for_only_HGT_genes(gff, LTG, dna_file, sd_numbers, rnaseq, out_file):
     """main function. This calls the other function to get a dictionary
     of genes on scaffolds. A list of HGT/LTG genes and check the scaffolds
     to identify those that only have HGT genes on them. If so, then this
@@ -84,11 +175,25 @@ def check_scaffolds_for_only_HGT_genes(gff, LTG, out_file):
     scaffold_to_gene_dict = parse_gff(gff)
     #call function to get gene_set, gene_to_comment_dict
     HGT_predicted_gene_set, gene_to_comment_dict = LTG_file(LTG)
-    #for scaffold in scaffold_to_gene_dict:
-        #bad_contig = True
-        #for gene in scaffold:
-            #if gene not in HGT_predicted_gene_set:
-                #bad_contig = False
+
+    #call funt gene_to_exon_count
+    gene_to_exon_count = gene_to_exon(gff)
+
+    #print gene_to_exon_count
+
+    #call function to get rna seq mapping TPM
+    gene_to_expression = parse_rnaseq(rnaseq)
+
+    #call function with DNA file
+    gene_AT_cont_dic, the_mean, standard_dev = get_stats_on_AT_content(dna_file)
+    print "the AVR AT = %f with SD %f " %(the_mean, standard_dev)
+    
+    for gene, comment in gene_to_comment_dict.items():
+        check_HGT_AT_vs_global_AT(gene_AT_cont_dic, the_mean,
+                                  standard_dev, gene, comment,
+                                  sd_numbers, gene_to_expression, gene_to_exon_count)
+        
+
     for scaffold, genes in scaffold_to_gene_dict.items():
         bad_contig = True
         for gene in genes:
@@ -141,6 +246,16 @@ parser.add_option("--gff", dest="gff", default="test.gff",
 parser.add_option("--LTG", dest="LTG", default="test_LTG_LGT_candifates.out",
                   help="LTG outfile. ",
                   metavar="FILE")
+parser.add_option("--dna", dest="dna", default=None,
+                  help="predicted cds nucleotide genes for AT content stats ",
+                  metavar="FILE")
+parser.add_option("-s", dest="sd_numbers", default=3,
+                  help="the number of stadard deviations away from the mean"
+                  " for identifying genes "
+                  " that differ from normal AT content. default=3")
+parser.add_option("-r", "--rnaseq", dest="rnaseq", default=None,
+                  help="RNAseq expression profile for genes. "
+                  " in format # Name	Length	TPM	NumReads ")
 
 parser.add_option("-o", "--out_file", dest="out_file", default="test_output.txt",
                   help="outfile to list the bad contigs")
@@ -152,6 +267,9 @@ parser.add_option("-o", "--out_file", dest="out_file", default="test_output.txt"
 gff = options.gff
 out_file = options.out_file
 LTG = options.LTG
+dna = options.dna
+sd_numbers = options.sd_numbers
+rnaseq =options.rnaseq
 
 
 #run the program
@@ -161,7 +279,10 @@ if not os.path.isfile(gff):
 
 if not os.path.isfile(LTG):
     sys_exit("Input LTG file not found: %s" % LTG)
+if not os.path.isfile(dna):
+    sys_exit("Input dna file not found: %s" % dna)
     
-check_scaffolds_for_only_HGT_genes(gff, LTG, out_file)
+    
+check_scaffolds_for_only_HGT_genes(gff, LTG, dna, sd_numbers, rnaseq, out_file)
 
 
