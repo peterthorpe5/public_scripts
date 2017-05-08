c4be5d2598f203bd1084e17cce8357da265a75cd
Peter Thorpe <peter.thorpe@hutton.ac.uk>
2017-05-08 10:02:50 +0100

    directions added

--- genomic_upstream_regions/re_format_GFF_Mcscanx.py
+++ genomic_upstream_regions/re_format_GFF_Mcscanx.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
-#os importsimport os
+# Title:
+# script to reformt GFF. for MCScanX, or
+# for genomic upstream regions python script
+# author: Peter Thorpe September 2016. The James Hutton Insitute, Dundee, UK.
+
+import os
 from sys import stdin,argv
 import sys
 from optparse import OptionParser
 
-#Title:
-#script to reformt GFF. for MCScanX
-
-#############################################################################
-#functions
+# function
 
 def reformat_gff_column_9(gff, species, out, mcscan):
-    "function to reformat name. Remove pipes and make names shorter"
+    """function to reformat name. Remove pipes and make names shorter"""
     f_in = open(gff, "r")
     f_out = open(out, 'w')
 
@@ -19,7 +20,8 @@ def reformat_gff_column_9(gff, species, out, mcscan):
         if line.startswith("#"):
             continue
         assert len(line.split("\t")) ==9 ,"GFF fields wrong length should be 9"
-        scaf,aug,cds_type,start,stop,e,f,g,gene_info = line.split("\t")
+        scaf, source, feature, start, stop, score, \
+              direction, frame, gene_info = line.split("\t")
         try:
             if mcscan == True:
                 scaf = scaf.split("scaffold_")[1]
@@ -30,13 +32,24 @@ def reformat_gff_column_9(gff, species, out, mcscan):
         gene_info = gene_info.replace("ID=", "").split()[0]
         gene_info = gene_info.split(".t")[0]
         gene_info = gene_info.replace(";", "")
-        #print "gene_info =", gene_info
-        #print "start = ", start
-        #print "stop =", stop
-        if not cds_type == "gene":
+        gene_info = gene_info.split("Note=")[0]
+        
+        if not feature == "gene":
             continue
-        data = "%s%s\t%s\t%s\t%s\n" %(species,scaf,gene_info,\
-                                    str(start), str(stop))
+        if mcscan == True:
+            data = "%s%s\t%s\t%s\t%s\n" %(species,
+                                          scaf,
+                                          gene_info,
+                                          str(start),
+                                          str(stop))
+        else:
+            # we want to add the direction information
+            data = "%s%s\t%s\t%s\t%s\t%s\n" %(species,
+                                              scaf,
+                                              gene_info,
+                                              str(start),
+                                              str(stop),
+                                              direction)
         f_out.write(data)
     f_out.close()
     f_in.close()
@@ -44,15 +57,17 @@ def reformat_gff_column_9(gff, species, out, mcscan):
 
 #################################################################################################
 if "-v" in sys.argv or "--version" in sys.argv:
-    print "v0.0.1"
+    print "v0.0.2"
     sys.exit(0)
 
 
 usage = """Use as follows:
 
-$ python re_format_gff_mcscan.py --gff augustus.gff -s Mc -o Gff_for_mcscan
+if reformatting gff for mcscanx type --mcscan.
+$ python re_format_gff_mcscan.py --gff augustus.gff -s Mc -o Gff_for_mcscan --mcscan
 
-
+else,
+this will pull the gene regions out of a gff
 """
 
 parser = OptionParser(usage=usage)
@@ -60,10 +75,13 @@ parser = OptionParser(usage=usage)
 parser.add_option("--gff", dest="gff", default=None,
                   help="hintsfile",
                   metavar="FILE")
-parser.add_option("-m", "--mcscan", dest="mcscan", default=False,
-                  help="specific formatting for Mcscan. Default is false, change to True if you need this. ",
+parser.add_option("-m", "--mcscan", dest="mcscan",
+                  default=False,
+                  help="specific formatting for Mcscan. Default is " +
+                  "false, change to True if you need this. ",
                   metavar="FILE")
-parser.add_option("-s", "--species", dest="species", default="Sp",
+parser.add_option("-s", "--species", dest="species",
+                  default="",
                   help="species prefix to add into column 1 two letters",
                   metavar="FILE")
 parser.add_option("-o", "--out", dest="out", default=None,
@@ -79,5 +97,11 @@ species = options.species
 out = options.out
 
 #run the program
-reformat_gff_column_9(gff, species, out, mcscan)
+# Run as script
+if __name__ == '__main__':
+    if not os.path.isfile(gff):
+        print("sorry cannot find you %s file" % gff)
+        os._exit(0)
+    print ("warning: GFF files vary! make sure you check the file before using")
+    reformat_gff_column_9(gff, species, out, mcscan)
 
