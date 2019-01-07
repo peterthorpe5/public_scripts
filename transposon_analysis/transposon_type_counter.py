bc7f1d76d01fa1ef825cbd1ba2675b6687973a84
peterthorpe5 <pjt6@st-andrews.ac.uk>
2019-01-07 12:01:53 +0000

    comm TE len outputted

--- transposon_analysis/transposon_type_counter.py
+++ transposon_analysis/transposon_type_counter.py
@@ -52,6 +52,11 @@ def parse_gff(gff):
     count = 1
     transposon_count_dict = defaultdict(int)
     note_dict = defaultdict(int)
+    # sum up the commumalitve lenght of a type of transpson in the genome
+    # why? to see if certaqin TE take up way more space than they should
+    # but that need more downstream analysis of course.
+    TE_comm_len = defaultdict(int) # default value of int is 0
+    TE_comm_len_name_annot = defaultdict(int) # default value of int is 0
 
     # iterate through gff
     for line in f_in:
@@ -64,8 +69,13 @@ def parse_gff(gff):
         Name_annot, note_annot = parseTransposon_info(gene_info)
         transposon_count_dict[Name_annot] += 1
         note_dict[note_annot] += 1
+        TE_comm_len[Name_annot] += (int(stop) - int(start))
+        # note annot is a much more refined type of TE 
+        TE_comm_len_name_annot[note_annot] += (int(stop) - int(start))
     f_in.close()
-    return transposon_count_dict, note_dict
+    #print(TE_comm_len)
+    #print(TE_comm_len_name_annot)
+    return transposon_count_dict, note_dict, TE_comm_len, TE_comm_len_name_annot
 
 
 if "-v" in sys.argv or "--version" in sys.argv:
@@ -108,7 +118,8 @@ if __name__ == '__main__':
     # call the main function
     if not os.path.isfile(transposons_gff):
         print("sorry, couldn't open the file: " % transposons_gff)
-    transposon_count_dict, note_dict = parse_gff(transposons_gff)
+    transposon_count_dict, note_dict, TE_comm_len, \
+                           TE_comm_len_name_annot = parse_gff(transposons_gff)
     outfile = open(out_file, "w")
 
     title = "#Transposon\tCount\n"
@@ -119,12 +130,34 @@ if __name__ == '__main__':
     outfile.close()
     
     ###################################
-    
-    note_out = out_file + "NOTES.txt"
+    # NOTE is the "more refined" type of TE
+    note_out = out_file.replace(".txt", "") + "_NOTES.txt"
     noteout = open(note_out, "w")
     title = "#Notes\tCount\n"
     noteout.write(title)
     for key, vals in note_dict.items():
+        outfmt = "%s\t%d\n" % (key, vals)
+        noteout.write(outfmt)
+    noteout.close()
+
+    ###################################
+    # output the commumalative len of the TE in the genome
+    note_out = out_file.replace(".txt", "") + "_commu_length_TE.txt"
+    noteout = open(note_out, "w")
+    title = "#Notes\ttotal_length\n"
+    noteout.write(title)
+    for key, vals in sorted(TE_comm_len.items()):
+        outfmt = "%s\t%d\n" % (key, vals)
+        noteout.write(outfmt)
+    noteout.close()
+    
+    ###################################
+    # output the commumalative len of the TE in the genome
+    note_out = out_file.replace(".txt", "") + "_commu_length_Family.txt"
+    noteout = open(note_out, "w")
+    title = "#Notes\ttotal_length\n"
+    noteout.write(title)
+    for key, vals in sorted(TE_comm_len_name_annot.items()):
         outfmt = "%s\t%d\n" % (key, vals)
         noteout.write(outfmt)
     noteout.close()
